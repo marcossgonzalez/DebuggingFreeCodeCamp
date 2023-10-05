8b67f69dcbb385c8501f8a3bb2c7aa0eac1526cb
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-10-04 19:42:35 -0700

    update lesson
    update lesson

---
+++ simpledebugging.js
@@ -0,0 +1,15 @@
+//-------------------------------------------------
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+
+
+let sumAB = a + b;
+console.log(sumAB);
+console.log(a)
+//-------------------------------------------------
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output);
+console.clear();
+//-------------------------------------------------
